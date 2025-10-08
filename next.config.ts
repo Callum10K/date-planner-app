f5c7daff086b90503205b5dbf8fe2bf8c43f402f
Callum10K <140648473+Callum10K@users.noreply.github.com>
2025-10-08 12:34:22 +0800

    Revert "Update next.config.ts"
    This reverts commit 4397e9dc7567b890e3f46cb6abd6f5a5f119eb3f.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: 'standalone',
-    appDir: true,
-    cleanDistDir: true,
 };
 
 export default nextConfig;
